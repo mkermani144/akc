7f78313dadb9122d4a7ccdc84cfc1acad9377c27
Mohammad Kermani <mkermani144@gmail.com>
2024-01-28 21:52:25 +0330

    fix: show error in case of a memory without any names

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,8 +1,8 @@
-use core::panic;
-use serde::{Deserialize, Serialize};
-use rand::prelude::*;
-use rand::distributions::WeightedIndex;
 use confy::{load, store};
+use core::panic;
+use rand::distributions::WeightedIndex;
+use rand::prelude::*;
+use serde::{Deserialize, Serialize};
 
 mod utils;
 
@@ -21,17 +21,16 @@ mod default_reduction {
 
 const CONFIG_NAME: &str = "akc";
 
-
 #[derive(Serialize, Deserialize)]
 pub struct FriendInfo {
     name: String,
     chance: f64,
-    level: String
+    level: String,
 }
 
 #[derive(Serialize, Deserialize, Default)]
 pub struct AkcConfig {
-    friends: Vec<FriendInfo>
+    friends: Vec<FriendInfo>,
 }
 
 pub fn read_config() -> AkcConfig {
@@ -47,7 +46,10 @@ fn add_friend(friend_info: FriendInfo) {
     let is_duplicate = utils::is_name_duplicate(&config, &friend_info.name);
 
     if is_duplicate {
-        println!("Name \"{}\" already exists, please use a different name", friend_info.name)
+        println!(
+            "Name \"{}\" already exists, please use a different name",
+            friend_info.name
+        )
     } else {
         config.friends.push(friend_info);
         write_config(config);
@@ -58,7 +60,7 @@ pub fn add_aji(name: String) {
     add_friend(FriendInfo {
         name,
         chance: default_chance::AJI,
-        level: "aji".to_owned()
+        level: "aji".to_owned(),
     })
 }
 
@@ -66,7 +68,7 @@ pub fn add_ki(name: String) {
     add_friend(FriendInfo {
         name,
         chance: default_chance::KI,
-        level: "ki".to_owned()
+        level: "ki".to_owned(),
     })
 }
 
@@ -74,7 +76,7 @@ pub fn add_chi(name: String) {
     add_friend(FriendInfo {
         name,
         chance: default_chance::CHI,
-        level: "chi".to_owned()
+        level: "chi".to_owned(),
     })
 }
 
@@ -88,20 +90,33 @@ pub fn suggest() {
     let filtered_config = utils::filter_config_by_enough_chance(&config);
     let mut rng = thread_rng();
 
-    let weighted_dist = WeightedIndex::new(filtered_config.iter().map(| friend_info | friend_info.chance)).expect("Failed to suggest a friend");
-    println!("Suggested friend: {}", config.friends[weighted_dist.sample(&mut rng)].name);
+    let weighted_dist =
+        WeightedIndex::new(filtered_config.iter().map(|friend_info| friend_info.chance))
+            .expect("Failed to suggest a friend");
+    println!(
+        "Suggested friend: {}",
+        config.friends[weighted_dist.sample(&mut rng)].name
+    );
 }
 
 fn add_memory(reduction: f64, names: &[String]) {
+    if names.is_empty() {
+        println!("Please specify at least one name");
+        return;
+    }
     let mut config = read_config();
     let unknown_names = utils::get_unknown_names(&config, names);
 
     if !unknown_names.is_empty() {
-        let unknown_names_string = unknown_names.iter()
-            .map(| &unknown_name | unknown_name.to_owned())
+        let unknown_names_string = unknown_names
+            .iter()
+            .map(|&unknown_name| unknown_name.to_owned())
             .collect::<Vec<String>>()
             .join(", ");
-        println!("The following names are not added yet: {}", unknown_names_string);
+        println!(
+            "The following names are not added yet: {}",
+            unknown_names_string
+        );
     } else {
         let total_reduction = reduction * names.len() as f64;
         let current_total_chance = utils::get_config_total_chance(&config, names);
